1a2a8cddeac6370247a018f9961a2183dbcba752
Jamie Wright <jamie@brilliantfantastic.com>
2010-11-03 07:27:42 -0400

    initial checkin

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Alice::Application.config.secret_token = 'ed9155be47f0617d253e0926e6263cbb7fc8be3e8ce20e3fbf463cf29ad9ae301334e7509510f173e1cfc571925df0d699c5df3f644b11bf5c6cef5e511f7918'
